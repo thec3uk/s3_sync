a7b9197c338f25de0e9da862d8ae385fb01e3436
Andrew Miller <info@akmiller.co.uk>
2020-03-20 15:47:22 +0000

    better script

---
+++ archive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SOURCE=$1
+LOCAL_ARCHIVE=$2
+WEBHOOK_URL=https://hooks.slack.com/services/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
+EMOJI=:ghost:
+USERNAME=archive-bot
+CHANNEL=#alerts-archive
+
+#cp -r $1 $2
+#aws s3 sync --quiet $2 s3://thec3-online-dump/
+TEXT=$(df -h | grep $2)
+curl -o /dev/null -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"${TEXT}\", \"icon_emoji\": \"${EMOJI}\"}" $WEBHOOK_URL 

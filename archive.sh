52d3247cfae3e7bf6181a2735a4509d1d56ad792
Andrew Miller <info@akmiller.co.uk>
2020-03-20 15:54:13 +0000

    fix the script and try to mount to begin with

--- archive.sh
+++ archive.sh
@@ -7,7 +7,9 @@ EMOJI=:ghost:
 USERNAME=archive-bot
 CHANNEL=#alerts-archive
 
-#cp -r $1 $2
-#aws s3 sync --quiet $2 s3://thec3-online-dump/
+mount_smbfs //admin:c3media@c3-streamer/Records $1 && \
+    cp -r $1 $2 && \
+    aws s3 sync --quiet $2 s3://thec3-online-dump/
+
 TEXT=$(df -h | grep $2)
 curl -o /dev/null -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"${TEXT}\", \"icon_emoji\": \"${EMOJI}\"}" $WEBHOOK_URL 

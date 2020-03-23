f86f1249603e5554a064594dd1a42ac0fc10063a
Andrew Miller <info@akmiller.co.uk>
2020-03-23 16:20:07 +0000

    fix script

--- archive.sh
+++ archive.sh
@@ -4,14 +4,13 @@ SOURCE=$1
 LOCAL_ARCHIVE=$2
 MUSER=$3
 MPASS=$4
+HOOK=$5
 
 slack_hook () {
-  # needs to be an ENV var outside of the script
-  # WEBHOOK_URL=https://hooks.slack.com/services/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
   EMOJI=:ghost:
   USERNAME=archive-bot
   CHANNEL=#alerts-archive
-  curl -o /dev/null -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"${1}\", \"icon_emoji\": \"${EMOJI}\"}" $WEBHOOK_URL
+   curl -o /dev/null -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"${1}\", \"icon_emoji\": \"${EMOJI}\"}" "$2"
 }
 
 mount_volumes () {
@@ -21,14 +20,16 @@ mount_volumes () {
   mount -t cifs //mediaserver01/vmix /media/vmix/ -o user=$1 -o pass=$2
 }
 
-mount_volumes $MUSER $MPASS
+# mount_volumes $MUSER $MPASS
 echo "Copying ${SOURCE} to ${LOCAL_ARCHIVE}" && \
     rsync -a --exclude=Windows10Upgrade --exclude='$RECYCLE.BIN' --exclude='System Volume Information' $SOURCE $LOCAL_ARCHIVE && \
     echo "Syncing ${LOCAL_ARCHIVE} to S3" && \
-    /usr/local/bin/aws s3 sync --quiet $LOCAL_ARCHIVE s3://thec3-online-dump/
+    /usr/bin/aws s3 sync --quiet $LOCAL_ARCHIVE s3://thec3-online-dump/
 
 
 TEXT=$(df -h | grep Filesystem)
-slack_hook $TEXT
+slack_hook "${TEXT}" "${HOOK}"
 TEXT=$(df -h | grep $2)
-slack_hook $TEXT
+slack_hook "${TEXT}" "$HOOK"
+
+exit 0

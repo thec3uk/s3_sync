6db0e642638ed70d6ebb7734caca331d8b173671
Andrew Miller <info@akmiller.co.uk>
2020-03-23 09:15:50 +0000

    improvements to the script

--- archive.sh
+++ archive.sh
@@ -2,14 +2,24 @@
 
 SOURCE=$1
 LOCAL_ARCHIVE=$2
-WEBHOOK_URL=https://hooks.slack.com/services/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
-EMOJI=:ghost:
-USERNAME=archive-bot
-CHANNEL=#alerts-archive
 
-mount_smbfs //admin:c3media@c3-streamer/Records $1
-cp -r $1 $2 && \
-    aws s3 sync --quiet $2 s3://thec3-online-dump/
+slack_hook () {
+  # needs to be an ENV var outside of the script
+  WEBHOOK_URL=https://hooks.slack.com/services/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
+  EMOJI=:ghost:
+  USERNAME=archive-bot
+  CHANNEL=#alerts-archive
+  curl -o /dev/null -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"${1}\", \"icon_emoji\": \"${EMOJI}\"}" $WEBHOOK_URL
+}
 
+/sbin/mount_smbfs //admin:c3media@c3-streamer/Records $SOURCE
+echo "Copying ${SOURCE} to ${LOCAL_ARCHIVE}" && \
+    cp -r $SOURCE $LOCAL_ARCHIVE && \
+    echo "Syncing ${LOCAL_ARCHIVE} to S3" && \
+    /usr/local/bin/aws s3 sync --quiet $LOCAL_ARCHIVE s3://thec3-online-dump/
+
+
+TEXT=$(df -h | grep Filesystem)
+slack_hook $TEXT
 TEXT=$(df -h | grep $2)
-curl -o /dev/null -s -X POST --data-urlencode "payload={\"channel\": \"${CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"${TEXT}\", \"icon_emoji\": \"${EMOJI}\"}" $WEBHOOK_URL 
+slack_hook $TEXT
